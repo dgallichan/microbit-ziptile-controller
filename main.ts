8afc0421d4813a450073c941548dd8e07d794723
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-20 17:07:00 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,11 +7,9 @@ basic.showLeds(`
     . # . # .
     `)
 basic.pause(2000)
+input.calibrateCompass()
 basic.forever(function () {
     radio.sendValue("Red", Math.map(input.compassHeading(), 0, 360, 0, 255))
-    radio.sendValue("Green", 0)
-    radio.sendValue("Blue", 0)
-    radio.sendValue("Bright", 50)
     serial.writeValue("x", input.compassHeading())
     led.plotBarGraph(
     input.compassHeading(),
