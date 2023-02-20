3048e0ef7602ae867b20b0a83e5b4c18bbe20280
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-20 16:50:47 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+radio.setGroup(200)
+basic.forever(function () {
+    radio.sendValue("Red", Math.map(input.compassHeading(), 0, 360, 0, 255))
+    radio.sendValue("Green", 0)
+    radio.sendValue("Blue", 0)
+    radio.sendValue("Bright", 50)
+    serial.writeValue("x", input.compassHeading())
+    led.plotBarGraph(
+    input.compassHeading(),
+    360
+    )
+})
